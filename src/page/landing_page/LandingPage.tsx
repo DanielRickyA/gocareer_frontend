68b1df08195fe2d34fe7570f303d635319f55b85
DanielRickyA <danielrickya1404@gmail.com>
2025-07-24 00:34:35 +0700

    Landing Page

--- src/page/landing_page/LandingPage.tsx
+++ src/page/landing_page/LandingPage.tsx
@@ -1,6 +1,5 @@
 // import photo1 from "../../assets/images/photo1.jpg";
 import { Card, CardContent } from "@/components/ui/card";
-import photo1 from "../../assets/images/Foto2.png";
 import FiturWebsite from "./FiturWebsite";
 import "./LandingPage.css";
 import { Button } from "@/components/ui/button";
@@ -8,28 +7,31 @@ function LandingPage() {
   return (
     <>
       <div className="relative " id="heroSectionId">
-        <Card className="bg-secondary p-0 px-10 pt-2 z-10">
+        <Card className="bg-secondary p-0 px-10 pt-2 z-10 shadow-none border-0">
           <CardContent className="p-0">
             <div className="flex flex-col md:flex-row items-start gap-10 h-full">
-              <div className="text-black w-full md:w-[80%] pt-8">
-                <h1 className="text-4xl font-semibold">
+              <div className="text-black w-full md:w-[80%] pt-8 pb-4">
+                <h1 className="text-4xl font-semibold ">
                   Temukan{" "}
                   <span className="text-primary">
                     Arah Pendidikan dan Jurusan Terbaikmu
                   </span>{" "}
                   di sini!
                 </h1>
-                <p className="text-base text-justify mt-4">
+                <p className="text-base text-justify mt-4 text-[#717A80]">
                   Go-Career membantumu memahami potensi diri, menemukan jurusan
                   yang sesuai, dan mendapat dukungan terbaik dari orang tua &
                   sekolah. Semua dalam satu platform.
                 </p>
-                <Button className="bg-primary rounded-full mt-4">
+                <Button
+                  className="bg-primary rounded-full mt-4 w-full md:w-auto text-[16px] font-medium"
+                  size={"lg"}
+                >
                   Tes Minat Sekarang
                 </Button>
               </div>
               <div className="h-full">
-                <img src={photo1} alt="photo1" className="" />
+                <img src="/Foto2.png" alt="photo1" className="" />
               </div>
             </div>
           </CardContent>
