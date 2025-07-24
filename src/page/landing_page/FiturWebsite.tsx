22592ef8c4ac66f588e2cc889a4eab93e02bf5a1
DanielRickyA <danielrickya1404@gmail.com>
2025-07-24 12:21:46 +0700

    123

--- src/page/landing_page/FiturWebsite.tsx
+++ src/page/landing_page/FiturWebsite.tsx
@@ -71,7 +71,7 @@ function FiturWebsite() {
           })}
         </div>
       </div>
-      <div className="absolute top-[10%] right-0 h-[15%] w-[30%] md:w-[15%] z-0">
+      <div className="absolute top-[10%] right-0 h-[15%] w-[30%] md:w-[10%] z-0">
         <img src="/assets1.png" alt="" className="w-full" />
       </div>
       <div className="absolute bottom-[0%] md:bottom-[40%] left-0 h-[15%] w-[30%] md:w-[15%] z-0">
