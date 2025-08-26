78d8374942b68a1c5732d94e4bb17359f295697a
DanielRickyA <danielrickya1404@gmail.com>
2025-08-26 23:33:15 +0700

    Perubahan Design dan Responsive

--- src/page/HasilTesMinat.tsx
+++ src/page/HasilTesMinat.tsx
@@ -1,4 +1,8 @@
+import { Card, CardContent } from "@/components/ui/card";
+import useIsMobile from "@/hooks/useIsMobile";
+import { CircleArrowLeft } from "lucide-react";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Tooltip,
   Legend,
@@ -10,7 +14,9 @@ import {
   PolarRadiusAxis,
 } from "recharts";
 function HasilTesMinat() {
+  const navigate = useNavigate();
   const [name, setName] = useState<string>("");
+  const isMobile = useIsMobile();
   const chartData = [
     {
       name: "Minat Ilmu Pengetahuan Alam",
@@ -42,7 +48,14 @@ function HasilTesMinat() {
   return (
     <div className="relative">
       <div className="container mx-auto max-w-6xl px-4 md:px-8 py-12 min-h-[85dvh]">
-        <div className="flex gap-6 items-center justify-start">
+        <div
+          className="flex justif-start items-center gap-2 cursor-pointer"
+          onClick={() => navigate("/uji-coba-minat")}
+        >
+          <CircleArrowLeft size={16} />
+          <p>Kembali</p>
+        </div>
+        {/* <div className="flex gap-6 items-center justify-start">
           <img src="/icon1.png" alt="" />
           <h1 className="text-4xl md:text-5xl text-left font-bold">
             Uji Coba Minat
@@ -51,42 +64,45 @@ function HasilTesMinat() {
         <p className="text-base md:text-xl mt-4 text-description text-justify">
           Kenali minat belajarmu lewat tes sederhana dan akurat. Biar kamu nggak
           salah pilih jurusan!
+        </p> */}
+        <p className="text-xl mt-4">
+          Hasil Uji Coba Minat untuk Peserta{" "}
+          <span className="font-semibold text-primary">{name}</span>.
         </p>
-        <p className="text-xl mt-8">
-          Hasil Uji Coba Minat Pada Peserta{" "}
-          <span className="font-semibold">{name}</span>
-        </p>
-        <p className="text-xl mt-6">
-          Minat Bakat yang cocok untuk Peserta{" "}
-          <span className="font-semibold">{name}</span> Adalah{" "}
-          <span className="font-semibold">Minat Ilmu Pengetahuan Alam</span>
-        </p>
-        <p className="text-xl mt-6">
-          Peserta yang memiliki Minat Bakat pada bidang{" "}
-          <span className="font-semibold">Minat Ilmu Pengetahuan Alam</span>{" "}
-          Kecenderungan pilihan peserta didik menyukai atau tidak menyukai
-          kegiatan bidang Ilmu Pengetahuan Alam
-        </p>
+        <Card className="bg-[#F9FAFC] flex flex-col h-full shadow-none border-[0.3px] w-full z-20 mt-4">
+          <CardContent>
+            <p className="text-xl font-semibold ">
+              Minat bakat yang paling sesuai untuk kamu:
+            </p>
+            <p className="text-4xl font-semibold mt-4 text-primary">
+              Minat Ilmu Pengetahuan Alam
+            </p>
+            <p className="text-xl mt-8">
+              Peserta dengan minat bakat di bidang{" "}
+              <span className="font-semibold text-primary">
+                Minat Ilmu Pengetahuan Alam
+              </span>
+              , Kecenderungan pilihan peserta didik menyukai atau tidak menyukai
+              kegiatan bidang Ilmu Pengetahuan Alam
+            </p>
+          </CardContent>
+        </Card>
         <p className="text-xl mt-6 font-semibold">
           Grafik Hasil Uji Coba Minat
         </p>
-        <div className="md:h-[450px] mt-4">
-          {/* {isLoading ? (
-            <Skeleton className="h-full w-full bg-gray-100 rounded-xl mb-6" />
-          ) : ( */}
+        <div className="h-[300px] md:h-[450px] mt-4">
           <ResponsiveContainer width="100%" height="100%">
             <RadarChart
-              cx="35%"
+              cx={`${isMobile ? "50%" : "35%"}`}
               cy="50%"
-              outerRadius="80%"
+              outerRadius={`${isMobile ? "50%" : "80%"}`}
               data={chartData}
-              margin={{ top: 0, right: 30, left: 20, bottom: 5 }}
             >
               <PolarGrid />
               <PolarAngleAxis dataKey="name" />
               <PolarRadiusAxis />
               <Tooltip />
-              <Legend layout="vertical" align="right" verticalAlign="middle" />
+              {/* <Legend layout="vertical" align="right" verticalAlign="middle" /> */}
               <Radar
                 name="Skor Minat"
                 dataKey="point"
@@ -96,7 +112,6 @@ function HasilTesMinat() {
               />
             </RadarChart>
           </ResponsiveContainer>
-          {/* )} */}
         </div>
       </div>
     </div>
