d6c8cda980ac5db18fe63456f1621bd4f0b02b14
DanielRickyA <danielrickya1404@gmail.com>
2025-08-30 16:20:10 +0700

    AS}PDIFbhnasuoiljfd;bn

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-export const BASE_URL = "http://localhost:3000/api/";
+// export const BASE_URL = "http://localhost:3000/api/";
+export const BASE_URL = "https://go-career-be.danielricky.my.id/api/";
 
 const useAxios = axios.create({
   baseURL: `${BASE_URL}`,
